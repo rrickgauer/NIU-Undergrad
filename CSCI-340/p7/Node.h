5e0b60d39fed93e2b322a5964b6cb6915c5d953b
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-01-10 14:21:20 -0600

    Create Node.h

---
+++ CSCI-340/p7/Node.h
@@ -0,0 +1,22 @@
+#ifndef H_NODE
+#define H_NODE
+
+// definition for class of nodes in bin tree
+
+template < class T > class binTree;  // forward declaration
+template < class T > class binSTree; // forward declaration
+
+template < class T >
+class Node {
+    friend class binTree < T >;          // binTree is friend
+    friend class binSTree < T >;         // binSTree is friend
+public:
+    // default constructor
+    Node ( const T& x = T ( ), Node < T >* l = 0, Node < T >* r = 0 ) :
+    data ( x ), left ( l ), right ( r ) { }
+private:
+    T data;                         // data component
+    Node < T > *left, *right;       // left and right links
+};
+
+#endif
