240ba016ee09ed920093ea11b01f395402d2d9ec
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-01-10 14:28:57 -0600

    Create prog9.h

---
+++ CSCI-340/p9/prog9.h
@@ -0,0 +1,15 @@
+#include "Entry.h"
+
+#ifndef H_PROG9
+#define H_PROG9
+
+#define SZ_BUF 80 // input buffer size
+
+// routines to read records
+void read_rec1 ( Entry& );
+void read_rec2 ( Entry& );
+
+// routine to print records
+void prnt_rec1 ( const Entry& );
+
+#endif
