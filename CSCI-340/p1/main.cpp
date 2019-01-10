374484d6c46b69c12a34063f1157fd351d384c26
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-01-10 14:12:20 -0600

    Create main.cpp

---
+++ CSCI-340/p1/main.cpp
@@ -0,0 +1,16 @@
+/*********************************************************
+ * Name:         Ryan Rickgauer
+ * Z-ID:         z-1729300
+ * Program #:    1
+ * Due Date:     9/6/2018
+ ********************************************************/
+#include "prg1.hpp"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    vector<int> v1;
+    genRndNums(v1);
+    printVec(v1);
+}
